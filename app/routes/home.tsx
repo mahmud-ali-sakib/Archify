fce9c9a07b0774b3af374456ce2bd05ba586dd67
grim-reaper883 <m.asakib05@gmail.com>
2026-03-28 15:02:21 +0600

    added project section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +14,80 @@ export default function Home() {
   return (
     <div className=" home">
       <Navbar />
-      <h1 className=" text-3xl text-indigo-500 font-extrabold">hello</h1>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse" />
+          </div>
+          <p>Introducing Archify</p>
+        </div>
+        <h1>Turn imagination into stunning spaces with Archify</h1>
+        <p className="subtitle">
+          An AI-first workspace built to help you design, visualize, and launch
+          architectural projects faster than ever.
+        </p>
+        <div className="actions ">
+          <a className="cta" href="#upload">
+            Start Building <ArrowRight className="icon" />
+          </a>
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3>Upload your floor paln</h3>
+              <p>Supports JPG, PNG formats up to 10MB</p>
+            </div>
+            <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one
+                place.
+              </p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="Project"
+                />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project New York</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("12.12.2026").toLocaleDateString()}</span>
+                    <span>By Sakib</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
